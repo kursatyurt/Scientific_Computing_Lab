ddf678fb31abe83cdb727a2ad35f7b91bcbbb7ed
Adrian Stobbe <stobbe@dive-solutions.de>
2020-11-25 10:07:21 +0100

    Add error reduction factor

--- WS2_Group12/PlotResults.m
+++ WS2_Group12/PlotResults.m
@@ -12,8 +12,11 @@ function [rks1,rks2,rks4,rks8,dt1,dt2,dt4,dt8] = PlotResults(name,func,exact_sol
 plot4(dt1,rks1,dt2,rks2,dt4,rks4,dt8,rks8,dt_exact,exact_sol,'Time','Function Value',name+' Solutions','\delta t=1','\delta t=1/2','\delta t=1/4','\delta t=1/8',name+'.png')
 disp("------------");
 disp("Approximation errors of "+name);
-ComputeError(rks1,ExactSol(dt1),1,t_end);
-ComputeError(rks2,ExactSol(dt2),1./2,t_end);
-ComputeError(rks4,ExactSol(dt4),1./4,t_end);
-ComputeError(rks8,ExactSol(dt8),1./8,t_end);
+E1 = ComputeError(rks1,ExactSol(dt1),1,t_end);
+E2 = ComputeError(rks2,ExactSol(dt2),1./2,t_end);
+disp("Error reduced by: "+  (E2/E1));
+E3 = ComputeError(rks4,ExactSol(dt4),1./4,t_end);
+disp("Error reduced by: "+  (E3/E2));
+E4 = ComputeError(rks8,ExactSol(dt8),1./8,t_end);
+disp("Error reduced by: "+  (E4/E3));
 end
