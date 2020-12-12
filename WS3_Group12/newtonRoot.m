0d55c0a667274f4cdd431db912143d6fba3c314a
Adrian Stobbe <stobbe@dive-solutions.de>
2020-12-12 09:55:56 +0100

    Added stop criterion newton

--- WS3_Group12/newtonRoot.m
+++ WS3_Group12/newtonRoot.m
@@ -1,7 +1,16 @@
 function y_new = newtonRoot(y_guess,accuracy,rhs,rhs_prime)
     error = realmax;
     y_new = y_guess;
+    n = 0;
+    max_n = 500;
     while(abs(error)>accuracy)
         y_new = y_new - rhs(y_new)/rhs_prime(y_new);
+        n = n + 1;
         error = rhs(y_new);
+%         disp(n);
+%         disp(abs(error));
+        if(n>max_n)
+            fprintf('Exceeded number of max iterations %i \n',max_n);
+            return;
+        end
     end
