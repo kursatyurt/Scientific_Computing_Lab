415e23757532fa3bf79b04588590dbb33e1c1f6c
Adrian Stobbe <stobbe@dive-solutions.de>
2020-12-12 10:46:15 +0100

    add plotting of solution

--- WS3_Group12/main.m
+++ WS3_Group12/main.m
@@ -15,9 +15,14 @@ rhs = matlabFunction(rhs); % symbolic functions are
 rhs_prime = matlabFunction(rhs_prime); 
 
 % solve
-input = {rhs,rhs_prime,y_0,dt,t_end};
-y = implicitEuler(input{:});
-y_a = adamsMoulton2(input{:});
-plot(t,y);
-% y_ana = 200./(20-10*exp(-7*t));
-% hold on, plot(t,y_ana,"r-");
+sol = Solutions(y_0,t_end,rhs,rhs_prime);
+dt_list = [1/2,1/4,1/8,1/16,1/32]';
+sz = length(dt_list);
+t_list = cell(1,sz);
+for i=1:sz
+    t_list{i} = 0:dt_list(i):t_end;
+end
+euler = getDtCell(sol,dt_list,@implicitEuler);
+adam = getDtCell(sol,dt_list,@adamsMoulton2);
+plotDtSolutions(t_list,dt_list,euler,'Implicit Euler');
+plotDtSolutions(t_list,dt_list,adam,'Adams Moulton 2nd order');
