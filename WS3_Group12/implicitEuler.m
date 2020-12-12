67ca2f05c3d16faa662b44ed50a434445694efbf
Adrian Stobbe <stobbe@dive-solutions.de>
2020-12-12 09:15:29 +0100

    Fast implicit euler

--- WS3_Group12/implicitEuler.m
+++ WS3_Group12/implicitEuler.m
@@ -4,9 +4,8 @@ function y = implicitEuler(rhs,rhs_prime,y_0,dt,t_end)
     y = zeros(nbr_steps,1);
     y(1) = y_0;
     for i=2:nbr_steps
-        syms r_root(x)
-        r_root(x) = y(i-1) - x + dt*rhs;
-        r_root_prime = diff(r_root,x);
-        y(i) = newtonRoot(y(i-1),accuracy,r_root,r_root_prime);
+          r_root = @(x) y(i-1) - x + dt*rhs(x);
+          r_root_prime = @(x) -1 + dt*rhs_prime(x);
+          y(i) = newtonRoot(y(i-1),accuracy,r_root,r_root_prime);
     end
 end
--- WS3_Group12/main.m
+++ WS3_Group12/main.m
@@ -1,15 +1,16 @@
 
 y_0 = 20;
 t_end = 5;
-dt = 1/8;
+dt = 1/64;
 t = 0:dt:t_end;
 
 syms rhs(x)
 rhs(x) = 7*(1-x/10)*x;
 rhs_prime = diff(rhs,x);
-
+rhs = matlabFunction(rhs); % symbolic functions are
+% very slow!
+rhs_prime = matlabFunction(rhs_prime); 
 y = implicitEuler(rhs,rhs_prime,y_0,dt,t_end);
 plot(t,y);
 y_ana = 200./(20-10*exp(-7*t));
-% hold on, plot(t,y_ana);
-% legend('t','y','t','y_ana');
+hold on, plot(t,y_ana,"r-");
