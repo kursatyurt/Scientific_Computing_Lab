33067084c811b98cb375b86e8d62eafa8b1737e1
bichphuong1204 <phuongnguyen@ut.ee>
2020-12-13 18:15:54 +0100

    modify worksheet2: using rhs, return vector y insteads of [x y], reduction factor inverse

--- WS2_Group12/Heun.m
+++ WS2_Group12/Heun.m
@@ -1,23 +1,18 @@
-function [x,y] = Heun(y0,x0,dt,finaltime)
+function y = Heun(y0,dt,finaltime)
+global rhs_pdot
 %Create Empty Vectors
-x=[];
 y=[];
 %Initialize the vectors
-x(1)= x0;
 y(1)= y0;
 %Initialize Counter
-time=x0;
 cnt=1;
 %Start Solution Loop
-	while time<finaltime
+for time=0:dt:(finaltime-dt)
 	  %Increment Counter
       cnt = cnt + 1;
 	  %Calculate next value of function
-      pdot_current = pdot(y(cnt-1));
-      pdot_next = pdot(y(cnt-1)+dt*pdot_current);
+      pdot_current = rhs_pdot(y(cnt-1));
+      pdot_next = rhs_pdot(y(cnt-1)+dt*pdot_current);
 	  y(cnt) = y(cnt-1) + dt/2*(pdot_current + pdot_next);
-	  %Increment time
-	  time = time + dt;
-	  x(cnt) = time;
     end
 end
--- WS2_Group12/worksheet2.m
+++ WS2_Group12/worksheet2.m
@@ -2,7 +2,13 @@ clc
 clear all
 close all
 
- %Exact Solution
+ % Define rhs function
+syms p pdot(y)
+eqn = pdot(p) == (1-y/10)*y;
+global rhs_pdot;
+rhs_pdot=matlabFunction(rhs(eqn));
+
+ % Exact Solution
 t_end = 5;
 dte=linspace(0,t_end,100);
 exsol=ExactSol(dte);
@@ -10,3 +16,5 @@ exsol=ExactSol(dte);
 Euler = PlotResults("Explicit-Euler",@ExplicitEuler,exsol,dte,t_end)
 Heun = PlotResults("Heun",@Heun,exsol,dte,t_end)
 RK = PlotResults("Runge-Kutta",@RK_4,exsol,dte,t_end)
+
+%ExplicitEuler(1,1/2,5)
