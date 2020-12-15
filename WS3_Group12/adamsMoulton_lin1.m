0e3522b56d5fef1eaf90b1bbb8c3589a95f1af30
kursatyurt <kursatyurt@gmail.com>
2020-12-15 20:56:32 +0100

    Corrected Linearization Methods

--- WS3_Group12/adamsMoulton_lin1.m
+++ WS3_Group12/adamsMoulton_lin1.m
@@ -6,7 +6,7 @@ function y = adamsMoulton_lin1(rhs,rhs_prime,y_0,dt,t_end)
     for i=2:nbr_steps
           prev_y = y(i-1);
           r_root = @(x) prev_y -x + dt/2 * (rhs(prev_y) + 7 * (1 - x/10) * prev_y );
-          r_root_prime = @(x) -1 + dt/2 * (-7/10 * prev_y * rhs_prime(x));
+          r_root_prime = @(x) -1+ dt/2 * (-7/10 * prev_y );
           y(i) = newtonRoot(y(i-1),accuracy,r_root,r_root_prime);
     end
 end
--- WS3_Group12/adamsMoulton_lin2.m
+++ WS3_Group12/adamsMoulton_lin2.m
@@ -6,7 +6,7 @@ function y = adamsMoulton_lin2(rhs,rhs_prime,y_0,dt,t_end)
     for i=2:nbr_steps
           prev_y = y(i-1);
           r_root = @(x) prev_y -x + dt/2 * (rhs(prev_y) + 7 * (1 - prev_y/10) * x );
-          r_root_prime = @(x) -1 + dt/2* 7 * ( 1 - prev_y/10 ) * rhs_prime(x);
+          r_root_prime = @(x) -1 + dt/2* 7 * ( 1 - prev_y/10 );
           y(i) = newtonRoot(y(i-1),accuracy,r_root,r_root_prime);
     end
 end
