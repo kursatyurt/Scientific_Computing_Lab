035bf42b7aa1faa8bc09e64f0c01d956b820fe26
bichphuong1204 <phuongnguyen@ut.ee>
2020-12-15 22:18:59 +0100

    update plots for explicit methods using Andrians templates

--- WS3_Group12/explicitEuler.m
+++ WS3_Group12/explicitEuler.m
@@ -1,16 +1,11 @@
-function y = explicitEuler(y0,dt,final_time, rhs)
-
-%Create Empty Vectors
-y= [];
-%Initialize the vectors
-y(1) = y0;
-%Initialize Counter
-cnt = 1;
-%Start Solution Loop
-for time = dt:dt:final_time
-      cnt = cnt + 1;
-	  %Calculate next value of function
-	  y(cnt) = y(cnt-1) + dt*rhs(y(cnt-1));
-	  %Increment time
+function y = explicitEuler(rhs, rhs_prime , y_0, dt, t_end)
+    
+    y = [];
+    y(1) = y_0;
+    cnt = 1;         
+    for time = dt:dt:t_end
+        cnt = cnt + 1;
+	    %Calculate next value of function
+	    y(cnt) = y(cnt-1) + dt*rhs(y(cnt-1));
     end
 end
