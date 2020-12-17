8e48b1b36ba0f22b3a69890590b9e6ec54212b4c
kursatyurt <kursatyurt@gmail.com>
2020-12-17 10:55:34 +0100

    Keep Function Sign Same

--- WS3_Group12/adamsMoulton_lin1.m
+++ WS3_Group12/adamsMoulton_lin1.m
@@ -1,4 +1,4 @@
-function y = adamsMoulton_lin1(y_0,dt,t_end)
+function y = adamsMoulton_lin1(rhs, rhs_prime , y_0, dt, t_end)
     global accuracy;
     nbr_steps = ceil(t_end/dt)+1;
     y = zeros(nbr_steps,1);
--- WS3_Group12/adamsMoulton_lin2.m
+++ WS3_Group12/adamsMoulton_lin2.m
@@ -1,8 +1,8 @@
-function y = adamsMoulton_lin2(y_0,dt,t_end)
+function y = adamsMoulton_lin2(rhs, rhs_prime , y_0, dt, t_end)
     global accuracy;
     nbr_steps = ceil(t_end/dt)+1;
     y = zeros(nbr_steps,1);
     y(1) = y_0;
     for i=2:nbr_steps
-            y(i) = (y(i-1)* (20 - 7 * dt * (y(i-1)-10)) / (7*dt*(y(i-1)-10)+20)
+            y(i) = (y(i-1)* (20 - 7 * dt * (y(i-1)-10))) / (7*dt*(y(i-1)-10)+20);
 end
