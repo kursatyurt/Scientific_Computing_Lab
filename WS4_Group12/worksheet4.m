e6811bce156f7f4a7630518d9d4138b6193613ea
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-01-09 11:20:33 +0100

    Clean up

--- WS4_Group12/worksheet4.m
+++ WS4_Group12/worksheet4.m
@@ -1,20 +1,32 @@
 clear all;
 clc;
 % resolution
-global nx ny;
 nx = 5;
 ny = nx;
-global hx2 hy2;
-hx2 = 1/(nx+1)^2;
-hy2 = 1/(ny+1)^2;
 %%%%
 
-A = getLaplacianMatrix(nx,ny);
+% b)
+% see getLaplacianMatrix
+
+% c)
+% see GS_solver
+
+% d.1)
+mat_sol = A\rhs;
+mat_sol = reshape(mat_sol,nx,ny)
+
+% d.2)
+A_sparse = sparse(A); % TODO build from scratch?
+mat_sol = A_sparse\rhs;
+
+% d.3)
+gauss = GS_solver(rhs,nx,ny)
+
 rhs_fct = @(x,y) -2*pi^2*sin(pi*x)*sin(pi*y);
 rhs = getFctVector(rhs_fct,nx,ny);
-num = linsolve(A,rhs);
-num = reshape(num,nx,ny)
-gauss = GS_solver(rhs,nx,ny)
+
+
+
 ana_fct = @(x,y) sin(pi*x)*sin(pi*y);
 ana = getFctVector(ana_fct,nx,ny);
 ana = reshape(ana,nx,ny)
