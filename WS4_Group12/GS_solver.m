e553016e27a1ccbc4ab4277e0d89ecaa2abe2aaa
kursatyurt <kursatyurt@gmail.com>
2021-01-09 03:57:41 +0100

    Added More Explonatory Comments

--- WS4_Group12/GS_solver.m
+++ WS4_Group12/GS_solver.m
@@ -1,46 +1,52 @@
 function T = GS_solver(rhs,nx,ny)
-
+%Start with zero initial values 
 T=zeros(nx,ny);
+%Calculate dx,dy and beta values 
 dx=1/(nx+1);
 dy=1/(ny+1);
 beta2=(dx/dy)^2;
+%Start with an infinitiy norm
 norm=inf;
 
-for k=1:1000
+for k=1:1000 %this loop should be changed to while norm > 1e-4
+
+%Start solution Loop
 add=0;
 for j=1:ny
     for i=1:nx
+        %Check for the boundaries 
         [left,right,up,down] = stencilcheck(i,j,nx,ny,T);
-        T(i,j)=  right + left +beta2*(up+down)-dx^2*rhs(i+add);
-        T(i,j)= T(i,j)/(2*(1+beta2));
+        %Apply Gauss-Seidel Formulation 
+        T(i,j)=  (right + left +beta2*(up+down)-dx^2*rhs(i+add))/(2*(1+beta2));
     end
     add = add + nx;
 end
+%Calculate norm for current solution 
 norm = calculate_norm(T,nx,ny,rhs);
 
 end
 end
 
 function [left,right,up,down] = stencilcheck(i,j,nx,ny,T)
-
+    %Check if on the left boundary.
     if i>1 
         left = T(i-1,j);
     else 
         left = 0;
     end
-    
-    if i<nx
+    %Check if on the left boundary 
+    if i<nx 
         right = T(i+1,j);
     else
         right = 0;
     end
-        
+    %Check if on the up boundary    
     if j>1
         up = T(i,j-1);
     else
         up = 0;
     end
-    
+    %Check if on the down boundary
     if j<ny
         down = T(i,j+1);
     else
