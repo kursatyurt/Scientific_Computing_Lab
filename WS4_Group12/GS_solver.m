153f233541932bc839841c0c9c240a620b74c292
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-01-09 10:34:02 +0100

    Fixed Norm issue

--- WS4_Group12/GS_solver.m
+++ WS4_Group12/GS_solver.m
@@ -8,9 +8,8 @@ beta2=(dx/dy)^2;
 %Start with an infinitiy norm
 norm=inf;
 
-% accuracy = 1e-4;
-% while(norm>accuracy)
-for k=1:1000 %this loop should be changed to while norm > 1e-4
+accuracy = 1e-4;
+while(norm>accuracy)
 %Start solution Loop
 add=0;
 for j=1:ny
@@ -35,7 +34,7 @@ function [left,right,up,down] = stencilcheck(i,j,nx,ny,T)
     else 
         left = 0;
     end
-    %Check if on the left boundary 
+    %Check if on the right boundary 
     if i<nx 
         right = T(i+1,j);
     else
@@ -68,16 +67,23 @@ end
 function norm = calculate_norm(T,nx,ny,rhs)
 dx=1/(nx+1);
 dy=1/(ny+1);
-%Reshape Matrix to vector
-x=reshape(T.',1,nx*ny);
 %Initialize norm and sums 
-norm=0;sum1=0;sum2=0;
+sum1=0;sum2=0;
 
 %Start Calculation
 n = nx*ny;
+i = 1;
+j = 0;
 for m=1:n
-    sum1 = (-2/dx^2 -2/dy^2)*x(m) + 1/dx^2*(boundCheck(x,m-1,n)+boundCheck(x,m+1,n)) +1/dy^2*(boundCheck(x,m-nx,n) + boundCheck(x,m+nx,n));
+    % convert m to i,j
+    if(mod(m,nx)==1)
+        i = 1;
+        j = j + 1;
+    end
+    [left,right,up,down] = stencilcheck(i,j,nx,ny,T);
+    sum1 = (-2/dx^2 -2/dy^2)*T(i,j) + 1/dx^2*(left+right) +1/dy^2*(up + down);
     sum2 = sum2 + (rhs(m) - sum1)^2;
+    i = i + 1;
 end
 %Calculate Norm
 norm = sqrt(1/(nx*ny)*sum2);
--- WS4_Group12/worksheet4.m
+++ WS4_Group12/worksheet4.m
@@ -2,22 +2,19 @@ clear all;
 clc;
 % resolution
 global nx ny;
-nx = 4;
-ny = 3;
-
-
+nx = 5;
+ny = nx;
 global hx2 hy2;
 hx2 = 1/(nx+1)^2;
 hy2 = 1/(ny+1)^2;
 %%%%
-nx = 5;
-ny = nx;
+
 A = getLaplacianMatrix(nx,ny);
 rhs_fct = @(x,y) -2*pi^2*sin(pi*x)*sin(pi*y);
 rhs = getFctVector(rhs_fct,nx,ny);
 num = linsolve(A,rhs)
 ana_fct = @(x,y) sin(pi*x)*sin(pi*y);
-ana = getFctVector(ana_fct,nx,ny);
+ana = getFctVector(ana_fct,nx,ny)
 
 T = GS_solver(rhs,nx,ny)
 
