1cc22388e9e58d7311ec09a0d42aa3f23b2b3a30
kursatyurt <kursatyurt@gmail.com>
2021-01-13 01:47:35 +0100

    Added a function to calculate Analytical Solution.

---
+++ WS4_Group12/getAnalyticalSolution.m
@@ -0,0 +1,10 @@
+function AnalyticalSolution = getAnalyticalSolution(nx,ny)
+%ana_fct = @(x,y) sin(pi*x)*sin(pi*y);
+%AnalyticalSolution = getFctVector(ana_fct,nx,ny);
+%AnalyticalSolution  = reshape(AnalyticalSolution ,nx,ny)
+
+[x,y] = meshgrid(linspace(1/(nx+1),1-1/(nx+1),nx),linspace(1/(ny+1),1-1/(ny+1),ny));
+AnalyticalSolution= sin(pi.*x).*sin(pi.*y);
+
+end
+
