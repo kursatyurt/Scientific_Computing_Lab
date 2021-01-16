880349ec767efc589534a8d88ddb25e29e9e309b
kursatyurt <kursatyurt@gmail.com>
2021-01-16 16:52:16 +0100

    Clean-Up Code // Add More Comments

--- WS4_Group12/getAnalyticalSolution.m
+++ WS4_Group12/getAnalyticalSolution.m
@@ -1,4 +1,5 @@
 function AnalyticalSolution = getAnalyticalSolution(nx,ny)
+%This Function Return nx by ny matrix that contains analytical solution expect the boundaries.
 
 [x,y] = meshgrid(linspace(1/(ny+1),1-1/(ny+1),ny),linspace(1/(nx+1),1-1/(nx+1),nx));
 AnalyticalSolution= sin(pi.*x).*sin(pi.*y);
--- WS4_Group12/make_plots.m
+++ WS4_Group12/make_plots.m
@@ -2,20 +2,24 @@ function make_plots(Nx_list,Ny_list, Data)
 % require list of Nx, and data computed from GS_solver
 
 for i=1:length(Nx_list)
+    %Get Mesh Size
     nx = Nx_list(i);
     ny = Ny_list(i);
+    %Get Solution
     sol=Data{i}{1};
+    %Include Boundaries
     sol_w_bounds = blkdiag(0,sol,0);
+    %Calcualte x,y values of mesh points for plotting
     [dimx,dimy] = meshgrid(linspace(0,1,ny+2),linspace(0,1,nx+2));
     
     figure;
     subplot(1,2,1);
-    surf(dimx,dimy, sol_w_bounds, 'FaceAlpha',0.95); colorbar; axis square;
-    title('Surface'); zlabel('Changes in temp');
+    surf(dimx,dimy, sol_w_bounds, 'FaceAlpha',0.95); colorbar; hcb=colorbar; hcb.Title.String = "Temperature";axis square;
+    title('Surface Plot'); xlabel('X');ylabel('Y');zlabel('Surface Temperature');
     
     subplot(1,2,2);
-    contour(dimx,dimy,sol_w_bounds,20,'--'); colorbar;axis square;
-    title('Contour');
+    contour(dimx,dimy,sol_w_bounds,20,'--'); colorbar; hcb=colorbar; hcb.Title.String = "Temperature";axis square;
+    title('Contour Plot'); xlabel('X');ylabel('Y');
 
 end
 
