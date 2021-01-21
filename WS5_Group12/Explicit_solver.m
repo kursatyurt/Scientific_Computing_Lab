fda6354e5b22cdc6c24d0aa55939378d34006eb8
kursatyurt <kursatyurt@gmail.com>
2021-01-21 04:28:14 +0100

    Added Explicit Euler Solver

---
+++ WS5_Group12/Explicit_solver.m
@@ -0,0 +1,22 @@
+function Tnp1 = Explicit_solver(Tn,nx,ny,dt)
+
+%Calculate lambdax, lambday and lambdax+lambday+1 values (i.e coefficients)_
+
+lambdax = dt/(nx+1)^2;
+lambday = dt/(ny+1)^2;
+lxly1  = -2*lambdax-2*lambday+1;
+
+%Take old solution as new one 
+Tnp1=Tn;
+
+%Solve the system
+ for j=2:ny+1
+     for i=2:nx+1
+         %Apply Explicit Formulation
+         Tnp1(i,j)= lambdax*(Tn(i-1,j)+Tn(i+1,j))+lambday*(Tn(i,j-1)+Tn(i,j+1))+lxly1*Tn(i,j);
+     end
+ end
+
+
+
+
