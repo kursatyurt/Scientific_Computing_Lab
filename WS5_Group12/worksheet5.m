2e95f4eba5ca002a9f0489263c06f3c93143926c
kursatyurt <kursatyurt@gmail.com>
2021-01-21 06:35:57 +0100

    Changes for Functions

--- WS5_Group12/worksheet5.m
+++ WS5_Group12/worksheet5.m
@@ -1,16 +1,13 @@
-
 clc;
 clear;
 close all;
 
-nx=15;
-ny=15;
-dt=1/8;
-Tn = blkdiag(0,ones(nx,ny),0);
-[dimx,dimy] = meshgrid(linspace(0,1,ny+2),linspace(0,1,nx+2));
 
-for time=0:dt:4/8
-    Tn = GS_solver(Tn,nx,ny,dt);
-end
+Nx_list=[3 7 15 31];
+Ny_list=Nx_list;
+dt_list=[1/64 1/128 1/256 1/512 1/1024 1/2048 1/4096];
 
-surf(dimx,dimy, Tn, 'FaceAlpha',0.95); colorbar; hcb=colorbar; hcb.Title.String = "Temperature";axis square;
+[sol_explicit,sol_gauss]=compute_AllinOne(Nx_list,Ny_list,dt_list)
+make_plots(Nx_list,Ny_list,dt_list,sol_explicit,"explicit_euler")
+make_plots(Nx_list,Ny_list,dt_list,sol_gauss,"gauss_seidel")
+
