8968cfa051e466a6e71687fb298e3b2a15f91aba
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-01-21 13:30:51 +0100

    nicerer plotting

--- WS5_Group12/make_plots.m
+++ WS5_Group12/make_plots.m
@@ -1,6 +1,8 @@
 function make_plots(Nx_list,Ny_list,dt_list,Data,methodname)
 for k=1:4 % Loop for time stations (1:4)/8 
 figure('WindowState','maximized');
+time = ["1/8" "2/8" "3/8" "4/8"];
+sgtitle(sprintf("t = %s",time(k)));
 dt_str=["1/64" "1/128" "1/256" "1/512" "1/1024" "1/2048" "1/4096"];
 l=1;
 for i=1:length(Nx_list)
@@ -20,7 +22,8 @@ ny = Ny_list(i);
     title(sprintf("Nx = %i, dt = %s",Nx_list(i),dt_str(j)))
 end
 end
+exportgraphics(gcf,methodname+num2str(k)+".pdf");
+close;
 end
-exportgraphics(gcf,"solution"+methodname+num2str(k)+".pdf");
 %close all;
 end
