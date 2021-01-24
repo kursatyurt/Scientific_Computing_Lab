00b64265421f436e847973e79ba9db0140be5ba3
kursatyurt <kursatyurt@gmail.com>
2021-01-24 14:47:00 +0100

    Corrected Norm and Iteration

--- WS5_Group12/GS_solver.m
+++ WS5_Group12/GS_solver.m
@@ -8,7 +8,7 @@ beta = 1/(2*lambdax+2*lambday+1);
 
 %Start with an infinity norm
 norm=inf;
-Tnp1=Tn;
+Tnp1=Tn; %Value at Next Iteration
 % Required accuracy 1e-6 take square of it 
 accuracy2 = 1e-12;
 
@@ -16,12 +16,17 @@ while(norm>accuracy2)
 %Apply Gauss-Seidel Formulation 
  for j=2:ny+1
      for i=2:nx+1
+<<<<<<< HEAD
          Tnp1(i,j)= (Tnp1(i,j)+lambdax*(Tnp1(i-1,j)+Tnp1(i+1,j))+lambday*(Tnp1(i,j-1)+Tnp1(i,j+1)))*beta;
+=======
+         Tnp1(i,j)= (Tn(i,j)+lambdax*(Tnp1(i-1,j)+Tnp1(i+1,j))+lambday*(Tnp1(i,j-1)+Tnp1(i,j+1)))*beta;
+>>>>>>> e46a739 (Corrected Norm and Iteration)
      end
  end
-
+ 
 %Calculate norm for current solution 
 norm = calculate_norm(Tnp1,nx,ny,lambdax,lambday,beta,Tn);
+
 end
 
 end
@@ -29,14 +34,15 @@ end
 
 %% Function to calculate residual norm
 function norm = calculate_norm(Tnp1,nx,ny,lambdax,lambday,beta,Tn)
-norm_m=zeros(nx*ny);
+norm_m=zeros(nx+2,ny+2);
 
  for j=2:ny+1
      for i=2:nx+1
-         norm_m(i,j)= beta*Tn(i,j)-lambdax*(Tnp1(i-1,j)+Tnp1(i+1,j))-lambday*(Tnp1(i,j-1)+Tnp1(i,j+1))-Tn(i,j);
+         norm_m(i,j)= Tn(i,j)+lambdax*(Tnp1(i-1,j)+Tnp1(i+1,j))+lambday*(Tnp1(i,j-1)+Tnp1(i,j+1))-(2*lambdax+2*lambday+1)*Tnp1(i,j);
      end
  end 
-norm=sum(norm_m(2:nx+1,2:ny+1),'all');
+norm_m=norm_m(2:nx+1,2:ny+1).^2;
+norm=sum(norm_m,'all')/nx/ny;
 end
 
 
