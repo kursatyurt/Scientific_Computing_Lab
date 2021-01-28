3b478e1b1eebe0e840c855c29a1d6c4cfc30ee5d
kursatyurt <kursatyurt@gmail.com>
2021-01-28 15:50:04 +0100

    Change function name // Change Implicit Solution Only for dt=1/64

--- WS5_Group12/worksheet5.m
+++ WS5_Group12/worksheet5.m
@@ -9,9 +9,9 @@ dt_list=[1/64 1/128 1/256 1/512 1/1024 1/2048 1/4096];
 output_times = (0:4)/8;
 
 sol_explicit=computeSolution(Nx_list,Ny_list,dt_list,output_times,@Explicit_solver);
-sol_gauss=computeSolution(Nx_list,Ny_list,dt_list,output_times,@GS_solver);
-save_plots(Nx_list,Ny_list,dt_list,sol_explicit,"Explicit Euler")
-save_plots(Nx_list,Ny_list,dt_list,sol_gauss,"Implicit Euler") 
-make_plots(Nx_list,Ny_list,dt_list,sol_explicit,"Explicit Euler")
-make_plots(Nx_list,Ny_list,dt_list,sol_gauss,"Gauss Seidel (Implicit Euler)")
+sol_gauss=computeSolution(Nx_list,Ny_list,1/64,output_times,@Implicit_solver);
+save_plots(Nx_list,Ny_list,dt_list,sol_explicit,"Explicit")
+save_plots(Nx_list,Ny_list,1/64,sol_gauss,"Implicit") 
+%make_plots(Nx_list,Ny_list,dt_list,sol_explicit,"Explicit")
+%make_plots(Nx_list,Ny_list,dt_list,sol_gauss,"Implicit")
 
